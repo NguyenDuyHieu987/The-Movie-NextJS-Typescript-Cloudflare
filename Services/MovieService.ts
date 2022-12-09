8d84ea4bfa53ab1563b2164b87a85e28f1256418
Dom Dom <vaicut6041@gmail.com>
2022-12-09 14:51:00 +0700

    add

--- Services/MovieService.ts
+++ Services/MovieService.ts
@@ -10,7 +10,7 @@ const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
 const TMDB_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p';
 const YOUTUBE_BASE_URL = 'https://www.youtube.com/watch';
 
-const URL_API = 'https://kind-pink-bluefish-cuff.cyclic.app';
+const URL_API = 'https://the-movie-node-content.onrender.com';
 // const URL_API = 'https://the-movie-node.onrender.com';
 const URL_API_IMAGE = 'https://the-movie-node-image.onrender.com';
 // const URL_API = 'http://localhost:3001';
